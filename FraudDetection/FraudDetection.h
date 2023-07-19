040ea53ceb26c8bba3c46aa62a0bb2dfe6bca86e
anirban-int <anirban.roy@indusnet.co.in>
2023-07-19 20:00:57 +0800

    project setup

---
+++ FraudDetection/FraudDetection.h
@@ -0,0 +1,18 @@
+//
+//  FraudDetection.h
+//  FraudDetection
+//
+//  Created by Appfactory on 19/07/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FraudDetection.
+FOUNDATION_EXPORT double FraudDetectionVersionNumber;
+
+//! Project version string for FraudDetection.
+FOUNDATION_EXPORT const unsigned char FraudDetectionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FraudDetection/PublicHeader.h>
+
+
