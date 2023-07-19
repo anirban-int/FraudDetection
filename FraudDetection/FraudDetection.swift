d9549e29aeed24c7f83e15e58826eb75da66a8ad
anirban-int <anirban.roy@indusnet.co.in>
2023-07-19 21:28:51 +0800

    added test cases

---
+++ FraudDetection/FraudDetection.swift
@@ -0,0 +1,23 @@
+//
+//  SwiftLib.swift
+//  FraudDetection
+//
+//  Created by Appfactory on 19/07/23.
+//
+
+import Foundation
+
+
+public final class FraudDetection {
+
+    let name = "FraudDetection"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
